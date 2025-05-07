6384d7eb1db8babc16ab179841f318221fa47532
oli-tolnai <oli.tolnai3@gmail.com>
2025-05-07 12:14:07 +0200

    import file drag and drop done

--- sztguiFeleves/ViewModels/MainWindowViewModel.cs
+++ sztguiFeleves/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace sztguiFeleves.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : INotifyPropertyChanged
     {
+        private string _filePath;
+        public string FilePath
+        {
+            get => _filePath;
+            set
+            {
+                if (_filePath != value)
+                {
+                    _filePath = value;
+                    OnPropertyChanged(nameof(FilePath));
+                }
+            }
+        }
+
+
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- sztguiFeleves/Views/MainWindow.xaml.cs
+++ sztguiFeleves/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using sztguiFeleves.ViewModels;
+using Path = System.IO.Path;
 
 namespace sztguiFeleves.Views
 {
@@ -19,9 +22,93 @@ namespace sztguiFeleves.Views
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly string[] SupportedFileExtensions = { ".mp4", ".webm", ".mkv", ".MP4" };
+
+        private MainWindowViewModel ViewModel => DataContext as MainWindowViewModel;
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
         }
+
+
+
+        //Drag And Drop method
+        private void InputFilePathTextBox_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            // Allow drag-and-drop only if the data contains file paths
+            e.Handled = true;
+            e.Effects = e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.Copy : DragDropEffects.None;
+        }
+
+        private void InputFilePathTextBox_PreviewDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                if (files.Length > 0)
+                {
+                    string filePath = files[0];
+                    string fileExtension = Path.GetExtension(filePath).ToLower();
+
+                    // Validate the file extension
+                    if (IsSupportedFileType(fileExtension))
+                    {
+                        InputFilePathTextBox.Text = filePath;
+
+                        // Ensure the DataContext is cast to MainWindowViewModel
+                        if (DataContext is MainWindowViewModel viewModel)
+                        {
+                            viewModel.FilePath = filePath;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Unsupported file type. Please select a valid video file.", "Invalid File", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                }
+            }
+        }
+
+        private bool IsSupportedFileType(string fileExtension)
+        {
+            return Array.Exists(SupportedFileExtensions, ext => ext == fileExtension);
+        }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Create and configure the OpenFileDialog
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Video Files (*.mp4;*.webm;*.mkv)|*.mp4;*.webm;*.mkv|All Files (*.*)|*.*",
+                Title = "Select a File to Convert"
+            };
+
+            // Show the dialog and check if the user selected a file
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                string fileExtension = Path.GetExtension(filePath).ToLower();
+
+                // Validate the file extension
+                if (IsSupportedFileType(fileExtension))
+                {
+                    InputFilePathTextBox.Text = filePath;
+                    if (DataContext is MainWindowViewModel viewModel)
+                    {
+                        viewModel.FilePath = filePath;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Unsupported file type. Please select a valid video file.", "Invalid File", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+
+        }
+
+
     }
 }
